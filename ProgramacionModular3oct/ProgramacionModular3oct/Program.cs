e3d73ad3a5728076e9c9ed66a8495b8e2a5d31d4
herazocatalina77-svg <herazocatalina77@gmail.com>
2025-10-03 09:15:38 -0500

    Modular

---
+++ ProgramacionModular3oct/ProgramacionModular3oct/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace ProgramacionModular
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MostrarMensaje();
+            MostrarMensajes("Mi nombre es Catalina");
+            MostrarMensajes("Tengo 18 años");
+            //Console.WriteLine($"Edad calculada: {Calcularedad()}");
+            MostrarMensajes($"Tengo {CalcularEdad(2025, 2006)}");
+        }
+            //Modulo1- Procedimiento sin parámetros
+        static void MostrarMensaje()
+        {
+          Console.WriteLine("Bienvenidos a la clase de programación modular");
+            MostrarMensajes("Estoy desde procedimiento sin parámetros");
+        }
+        //Módulo 2 - Procedimiento con parámetros
+        static void MostrarMensajes(string mensaje)
+        {
+            Console.WriteLine(mensaje);
+        }
+        //Módulo 3 - Funciones
+        static int Calcularedad()
+        {
+            int edad;
+            edad = 2025 - 2006;
+            return edad;
+        }
+        //Módulo 4 - Función con parámetros
+         static int CalcularEdad(int añoActual, int añoNacimiento)
+         {
+            return añoActual - añoNacimiento;
+         }
+    }
+}
