5fb9c3099deba6984a957d98e14578717e66d5dd
herazocatalina77-svg <herazocatalina77@gmail.com>
2025-10-07 08:45:40 -0500

    modulosparte1

---
+++ Módulo13Oct/Módulo13Oct/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Modulo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] multiplosde5 = new int[CapturarFilas(), CapturarColumnas()];
+        }
+        static int[,] LlenarMatriz(int[,] matriz)
+        {
+            return matriz;
+        }
+        static int CapturarFilas()
+        {
+            Console.WriteLine("Ingrese el número de filas para la matriz:");
+            int filas=Convert.ToInt32(Console.ReadLine());
+            return filas;
+        }
+        static int CapturarColumnas()
+        {
+            Console.WriteLine("Ingrese el número de Columnas para la matriz:");
+            int filas = Convert.ToInt32(Console.ReadLine());
+            return filas;
+        }
+    }
+}
