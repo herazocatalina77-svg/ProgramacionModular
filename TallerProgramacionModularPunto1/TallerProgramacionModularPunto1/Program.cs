846014d79ac66cf67709f27c8ad42fc2b6ac4241
herazocatalina77-svg <herazocatalina77@gmail.com>
2025-10-15 12:25:08 -0500

    Punto1TallerMódulos

---
+++ TallerProgramacionModularPunto1/TallerProgramacionModularPunto1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace TallerModulosPunto1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Modulos
+            //Ingresa una palabra
+            //Llevar la palabra, vector de caracteres
+            //Contar la cantidad de consonantes y vocales de la palabra
+            OrganizarPalabra();
+
+        }
+        static char[] OrganizarPalabra() //Funcion para la palabra
+        {
+            string palabra; //Variable de la palabra a ingresar
+            Console.WriteLine("Ingrese una palabra");
+            palabra = Console.ReadLine();
+            //Array que tenga cada una de las letras de la palabra ingresada
+            int contadorvocales = 0;
+            int contadorconsonantes = 0;
+            char[] arraypalabra = palabra.ToCharArray(); //Convertir el string en array de chars
+            //Recorrer el array
+            for (int i = 0; i < arraypalabra.Length;i++)
+            {
+                Console.WriteLine(arraypalabra[i]);
+                //Condicional para las vocales y las consonantes
+                //Contadores de las vocales y consonantes
+                if (arraypalabra[i] == 'a' || arraypalabra[i] == 'e' || arraypalabra[i] == 'i'|| arraypalabra[i] == 'o' || arraypalabra[i] == 'u')
+                {
+                    contadorvocales++;
+                }
+                else
+                {
+                    contadorconsonantes++;
+                }
+                //Escribir la cantidad de vocales
+            }
+                Console.WriteLine($"La cantidad de vocales fueron {contadorvocales}");
+                Console.WriteLine($"La cantidad de consonantes fueron {contadorconsonantes}");
+            return arraypalabra;
+        }
+    }
+}
